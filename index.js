4f0a8233ea0c31f999be070b871ace95b8c2a045
denizkiskanc <denizkiskancc@gmail.com>
2022-05-04 16:31:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const game = {
+    player: 'Player 1',
+    score: 0,
+    time: null,
+    action: () => {
+        game.time = setTimeout(() => {
+            game.over();
+          }, 3000);
+    },
+    over: () => {
+        const lose = document.getElementById('lose');
+        lose.style.display = "block";
+    },
+    clear: () => {
+        clearTimeout(game.time);
+    },
+    increment: () => {
+        game.score++;
+        if (game.score == 2 ) {
+            const win = document.getElementById('win');
+            win.style.display = "block";
+        }
+    }
+
+}
